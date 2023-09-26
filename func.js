febdafc39248ae620a7e2d65401e84444c5de836
kim <gayun53a@naver.com>
2023-09-26 21:59:41 +0900

    1st commit

---
+++ func.js
@@ -0,0 +1,15 @@
+const {odd, even} = require('./var');
+
+function checkOddEven(number){
+    if (number%2){
+        return odd;
+    } else {
+        return even;
+    };
+};
+
+module.exports = {
+    checkOddEven,
+    odd,
+    even,
+};
---
+++ index.js
@@ -0,0 +1,5 @@
+const {odd, even} = require('./var');
+const checkOddEven = require('./func');
+
+console.log(checkOddEven);
+console.log()
